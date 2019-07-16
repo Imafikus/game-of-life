13fcf5c403e6442aabd1c76e8f4d5bfdc5201e33
Aleksa Tesic <tesic@getpenta.com>
2019-07-17 00:51:13 +0200

    Additonal changes

--- src/GolCanvas.java
+++ src/GolCanvas.java
@@ -8,16 +8,24 @@ import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 
 public class GolCanvas extends Application {
 
     static int width = 300;
     static int height = 300;
     static int cellSize = 10;
-    static int borderOffset = 1;
-    static Color emptyCellColor = Color.WHITE;
-    static Color fullCellColor = Color.BLUE;
-    static int numberOfCells = 10;
+
+    static Color deadCellColor = Color.WHITE;
+    static Color aliveCellColor = Color.BLUE;
+
+    static int numberOfRandomCells = 50;
+
+    static boolean[][] cellInfo = new boolean[width / cellSize][height / cellSize];
+
+    static boolean ALIVE = true;
+    static boolean DEAD = false;
 
     @Override
     public void start(Stage stage) {
@@ -25,21 +33,43 @@ public class GolCanvas extends Application {
     }
 
     private void init(Stage stage) {
-        Pane root = new Pane();
 
+        // Init Canvas stuff
+        Pane root = new Pane();
         Canvas canvas = new Canvas(width, height);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        setRandomCells(gc, numberOfCells);
-        drawGrid(gc);
 
-        root.getChildren().addAll(canvas);
+        // GOL init
+        initCellInfoMatrix(cellInfo);
+        setRandomCellsToAlive(gc, numberOfRandomCells);
 
-        Scene scene = new Scene(root, width, height, Color.WHITE);
 
-        stage.setTitle("Game of Life");
-        stage.setScene(scene);
-        stage.show();
+        //GOL loop
+        int n = 2;
+        while(n > 0) {
+            for (int i = 0; i < cellInfo.length; i++) {
+                for (int j = 0; j < cellInfo.length; j++) {
+                    updateCell(i, j);
+                    drawRectangle(gc, i, j, cellInfo[i][j]);
+                }
+            }
+            drawGrid(gc);
+            root.getChildren().addAll(canvas);
+            Scene scene = new Scene(root, width, height, Color.WHITE);
+            stage.setTitle("Game of Life");
+            stage.setScene(scene);
+            stage.show();
+            n--;
+        }
+
+        // display logic
+//        drawGrid(gc);
+//        root.getChildren().addAll(canvas);
+//        Scene scene = new Scene(root, width, height, Color.WHITE);
+//        stage.setTitle("Game of Life");
+//        stage.setScene(scene);
+        //stage.show();
     }
 
     private void drawGrid(GraphicsContext gc) {
@@ -59,30 +89,102 @@ public class GolCanvas extends Application {
 
     }
 
-    private void drawRectangle(GraphicsContext gc, int x, int y) {
+    private void drawRectangle(GraphicsContext gc, int x, int y, boolean cellType) {
 
-        gc.setFill(fullCellColor);
+        if(cellType == DEAD) {
+            gc.setFill(deadCellColor);
+        } else {
+            gc.setFill(aliveCellColor);
+        }
+
+
         gc.fillRect(x, y, cellSize, cellSize);
 
     }
 
-    private void setRandomCells(GraphicsContext gc, int numberOfCells) {
+    private void setRandomCellsToAlive(GraphicsContext gc, int numberOfCells) {
         for(int i = 0; i < numberOfCells; i++) {
 
             int[] coords = getRandomCoordinates();
             int x = coords[0];
             int y = coords[1];
 
-            drawRectangle(gc, x, y);
+            drawRectangle(gc, x, y, ALIVE);
+
+            cellInfo[x / cellSize][y / cellSize] = ALIVE;
         }
     }
 
     private int[] getRandomCoordinates() {
-        int x = (int)(Math.random() * 30) * 10;
-        int y = (int)(Math.random() * 30) * 10;
+        int x = (int)(Math.random() * 30) * cellSize;
+        int y = (int)(Math.random() * 30) * cellSize;
 
         int[] toReturn = {x, y};
         return  toReturn;
+    }
+
+    private void initCellInfoMatrix(boolean[][] cellInfo) {
+        for (int i = 0; i < width / cellSize ; i++) {
+            for(int j = 0; j < height / cellSize; j++) {
+                cellInfo[i][j] = DEAD;
+            }
+        }
+    }
+
+
+    private void updateCell (int x, int y) {
+        int cellNeighbours = getNumberOfAliveNeighbours(x, y);
+
+        // check solitude
+        if(cellNeighbours <= 1) cellInfo[x][y] = DEAD;
+
+        // check overpopulation
+        else if(cellNeighbours >= 4) cellInfo[x][y] = DEAD;
+
+        // check new life
+        else if(cellInfo[x][y] == DEAD && cellNeighbours == 3) cellInfo[x][y] = ALIVE;
+
+        // if we stay alive
+
+    }
+
+    private int getNumberOfAliveNeighbours(int x, int y) {
+        int alive = 0;
+
+        //upper left
+        if(x - 1 >= 0 && y - 1 >= 0 && cellInfo[x - 1][y - 1] == ALIVE) alive++;
+
+        //up
+        if(y - 1 >= 0 && cellInfo[x][y - 1] == ALIVE) alive++;
+
+        //upper right
+        if(x + 1 <= width && cellInfo[x + 1][y] == ALIVE) alive++;
+
+        //left
+        if(x - 1 >= 0 && cellInfo[x - 1][y] == ALIVE) alive++;
+
+        //right
+        if(x + 1 <= width && cellInfo[x + 1][y] == ALIVE) alive++;
+
+        // bottom left
+        if(x - 1 >= 0 && y + 1 <= height  && cellInfo[x - 1][y + 1] == ALIVE) alive++;
+
+        //  bottom
+        if(y + 1 <= height  && cellInfo[x][y + 1] == ALIVE) alive++;
+
+        //  bottom right
+        if(x + 1 <= width && y + 1 <= height  && cellInfo[x + 1][y + 1] == ALIVE) alive++;
+
+        return alive;
+    }
+
+    private void printCellInfoMatrix(){
+        for(int i = 0; i < cellInfo.length; i++) {
+            for(int j = 0; j < cellInfo.length; j++) {
+                System.out.print(cellInfo[i][j] + " ");
+            }
+        }
+        System.out.println(System.lineSeparator());
     }
 
     public static void main(String[] args) {
